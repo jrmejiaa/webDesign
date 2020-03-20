ca47b335454bb261ea97043088461e497d340fd6
Unknown <jairo.mejia.2708@gmail.com>
2020-03-19 20:41:50 -0500

    JavaScript First Steps

---
+++ 9_Learning_JavaScript/js/scripts.js
@@ -0,0 +1,14 @@
+    // There are three ways to create a variable 
+    var nombre = "Jairo Mejia";
+    // In JavaScript it is possible to change the type of the variable. It is very flexible to make those changes
+    console.log(nombre);
+
+    let name;
+    console.log(name);
+
+    let name = "Jairo Mejia";
+
+    let html =  `<ul>
+                    <li> ${name}
+                    <li> Mi nombre es ${name}
+                </ul>`
