b53eda793da050d50effb87a34c8c9da98d6e356
Unknown <jairo.mejia.2708@gmail.com>
2020-04-11 17:29:13 -0500

    First Steps with AJAX

---
+++ 13_AJAX/servidor.php
@@ -0,0 +1,9 @@
+<?php 
+    $technology = array(
+        'frontend'  => 'HTML5, CSS3, JavaScript',
+        'backend'   => 'PHP, MySQL, Apache',
+        'fullstack' => 'HTML5, CSS3, JavaScript, PHP, MySQL, Apache'
+    );
+
+    echo json_encode($technology);
+?>
